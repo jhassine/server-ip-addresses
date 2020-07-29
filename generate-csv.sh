d3baff035f20070ee9c9a2d6d077767ba3d15710
Jukka Hassinen <jukka.hassinen@gmail.com>
2020-07-29 22:00:58 +0300

    Fixes

--- generate-csv.sh
+++ generate-csv.sh
@@ -14,6 +14,6 @@ wget -qO- https://www.cloudflare.com/ips-v4 >> datacenters.csv
 
 wget -qO- https://www.gstatic.com/ipranges/cloud.json | grep -o "$CIDR_REGEX" >> datacenters.csv
 
-wget -qO- $(wget -qO- -U Mozilla https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519 | grep -Eo 'https://download.microsoft.com/download/\S+?\.json' | head -n 1) | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,\}' >> datacenters.csv
+wget -qO- $(wget -qO- -U Mozilla https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519 | grep -Eo 'https://download.microsoft.com/download/\S+?\.json' | head -n 1) | grep -o "$CIDR_REGEX" >> datacenters.csv
 
 echo "Success!"
