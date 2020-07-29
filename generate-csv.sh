295d719e93af2ee2be88c98bee1c88c07e299c04
Jukka Hassinen <jukka.hassinen@gmail.com>
2020-07-29 23:50:37 +0300

    Sorting CIDRs and fixed bug

--- generate-csv.sh
+++ generate-csv.sh
@@ -6,13 +6,15 @@ CIDR_REGEX='[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,\}'
 
 cd /data
 
-wget -qO- https://ip-ranges.amazonaws.com/ip-ranges.json | grep -o "$CIDR_REGEX" >> datacenters.txt
+wget -qO- https://ip-ranges.amazonaws.com/ip-ranges.json | grep -o "$CIDR_REGEX" > datacenters.txt
 
-wget -qO- https://www.cloudflare.com/ips-v4 | sed 's/.*/"&"/' >> datacenters.txt
+wget -qO- https://www.cloudflare.com/ips-v4 >> datacenters.txt
 
 wget -qO- https://www.gstatic.com/ipranges/cloud.json | grep -o "$CIDR_REGEX" >> datacenters.txt
 
 wget -qO- $(wget -qO- -U Mozilla https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519 | grep -Eo 'https://download.microsoft.com/download/\S+?\.json' | head -n 1) | grep -o "$CIDR_REGEX" >> datacenters.txt
+
+cat datacenters.txt | sort -V > datacenters.txt
 
 echo '"cidr"' > datacenters.csv
 
